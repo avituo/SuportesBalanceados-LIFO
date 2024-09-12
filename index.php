97418b503b4f417b0e318e9cce9a0613dcf64a44
Vitor Pereira <a.vitu_@outlook.com>
2024-09-12 15:22:27 -0300

    Implementing logic.

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
 <?php
 
-echo 'Hello World!';
+function suportesBalanceados($str) {
+    $map = array(
+        ')' => '(',
+        '}' => '{',
+        ']' => '['
+    );
+
+    $stack = array();
+
+    for ($i = 0; $i < strlen($str); $i++) {
+        $char = $str[$i];
+
+        if (in_array($char, $map)) {
+            $stack[] = $char;
+        } else if (isset($map[$char])) {
+            if (empty($stack) || array_pop($stack) != $map[$char]) {
+                return false;
+            }
+        }
+    }
+
+    return empty($stack);
+}
+
+//EXEMPLOS
+var_dump(suportesBalanceados("(){}[]"));
+var_dump(suportesBalanceados("[{()}](){}"));
+var_dump(suportesBalanceados("[]{()"));
+var_dump(suportesBalanceados("[{)]"));
